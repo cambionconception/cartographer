16571b3216b79779fb218ef53c9d3ba6be684b19
Joe <josey@berkeley.edu>
2023-10-20 13:03:24 -0700

    organization

---
+++ script.js
@@ -0,0 +1,4 @@
+var map = L.map('map', {center: [51.505, -0.09], zoom: 13});
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(mymap);
