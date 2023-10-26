f5431ab87327108fff7766f9fb8d1e94ed5205c1
Joe <josey@berkeley.edu>
2023-10-26 16:37:57 -0700

    navigation interface

--- root/js/script.js
+++ root/js/script.js
@@ -1,4 +1,7 @@
+
 var map = L.map('map', {center: [51.505, -0.09], zoom: 13});
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(mymap);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
