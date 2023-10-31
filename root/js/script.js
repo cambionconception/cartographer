cc04d6b857b99aa91eded59e908a5c34baf2f342
Joe <josey@berkeley.edu>
2023-10-30 23:37:24 -0700

    about page

--- root/js/script.js
+++ root/js/script.js
@@ -2,6 +2,6 @@
 var map = L.map('map', {center: [51.505, -0.09], zoom: 13});
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 5,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
